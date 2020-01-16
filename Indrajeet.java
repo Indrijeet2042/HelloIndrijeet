999d60e896b43044a9aeb6cbe8b742ca3534800f
Indrijeet Kumar <indriku1@in.ibm.com>
2020-01-16 18:12:36 +0530

    java folder commited

---
+++ Indrajeet.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Indrajeet{
+  public static void main(String[] args)
+               {
+                   System.out.println("Hellow Indrijeet");
+                }
+      }
